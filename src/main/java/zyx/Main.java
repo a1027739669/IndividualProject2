66f8b7e36900199f8dd32146948300b6e3cfcc1b
Komorebi <1027739669@qq.com>
2019-09-14 08:43:41 +0800

    完成step0，输出某个英文文本文件中 26 字母出现的频率，由高到低排列，并显示字母出现的百分比，精确到小数点后面两位。

--- src/main/java/zyx/Main.java
+++ src/main/java/zyx/Main.java
@@ -1,13 +1,27 @@
 package zyx;
 
+import zyx.func.Step0;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
  */
 public class Main
 {
-    public static void main( String[] args )
+    public static void main ( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        long start=System.currentTimeMillis();
+      Step0 step0=new Step0();
+      step0.cout("GameOfThron.txt");
+      long end=System.currentTimeMillis();
+        System.out.println(end-start);
     }
 }
---
+++ src/main/java/zyx/func/Step0.java
@@ -0,0 +1,58 @@
+package zyx.func;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Step0 {
+    private int[] letters = new int[26];
+    private float[] rate = new float[26];
+
+    public int[] countLetter(String filepath) throws IOException {
+        List<String> lists = Files.readAllLines(Paths.get(filepath));
+        int[] letters = new int[26];
+        int count = 0;
+        for (String list : lists) {
+            for (int i = 0; i < list.length(); i++) {
+                char c = Character.toUpperCase(list.charAt(i));
+                if (Character.isLetter(c)) {
+                    letters[c - 'A']++;
+                }
+            }
+        }
+        return letters;
+    }
+
+    public float[] countRate(int[] le) {
+        float[] ra = new float[26];
+        int count = 0;
+        for (int i = 0; i < le.length; i++) {
+            count += le[i];
+        }
+        for (int i = 0; i < ra.length; i++) {
+            ra[i] = (float) le[i] / count;
+        }
+        return ra;
+    }
+
+    public void cout(String filepath) throws IOException {
+        this.letters = this.countLetter(filepath);
+        this.rate = this.countRate(this.letters);
+        boolean[] flag = new boolean[26];
+        for (int i = 0; i < 26; i++) {
+            int position = 0;
+            int max=-1;
+            for (int j = 0; j < 26; j++) {
+                if ((this.letters[j] > max) && !flag[j]) {
+                    max=this.letters[j];
+                    position = j;
+                }
+            }
+            if (!flag[position]) {
+                System.out.printf("%c:频率:%f 百分比:%.2f%% 频数:%d\n", (char) (position + 'A'), this.rate[position], this.rate[position] * 100,this.letters[position]);
+                flag[position] = true;
+            }
+        }
+    }
+}
